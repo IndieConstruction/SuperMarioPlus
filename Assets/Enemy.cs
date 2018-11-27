73d891d7d680d8a8c0f1bf5cb8614366ea0493a6
Brago <brago@indieconstruction.com>
2018-11-27 17:27:26 +0100

    Project upload

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public string EnemyName = "Enemy To Rename";
+    public int Life = 10;
+    public float MoveSpeed = 1.4f;
+    private bool IsAlive = false;
+
+	// Use this for initialization
+	private void Start () {
+        bool printResult = PrintString("Start enemy");
+        Debug.Log("Print result: " + printResult);
+    }
+
+    // Update is called once per frame
+    private void Update () {
+        string tempString = "Update enemy";
+        PrintString(tempString);
+    }
+
+    private bool PrintString(string stringToPrint) {
+        if (EnemyName == "") {
+            return false;
+        } else {
+            Debug.Log(stringToPrint + " : " + EnemyName);
+            return true;
+        }
+
+    }
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int Life;
+    public int MovementSpeed;
+    public int Coins;
+    public int JumpForce;
+    public bool isGrounded;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    /// <summary>
+    /// Controllo input movimento player
+    /// </summary>
+    public void CheckInput()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-MovementSpeed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(+MovementSpeed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.W) && isGrounded == true)
+        {
+            transform.position = transform.position + new Vector3(0f, JumpForce *Time.deltaTime, 0f);
+            isGrounded = false;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Accoviacciamento");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.tag == "Plane")
+        {
+            isGrounded = true;
+            Debug.Log("collide");
+        }
+    }
+
+}
