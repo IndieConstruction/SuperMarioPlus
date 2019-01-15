8597c3c183dc307882ed04bc668461a50efef3db
Brago <brago@indieconstruction.com>
2019-01-15 18:26:32 +0100

    Aggiunta UI

---
+++ Assets/UI/Window/WindowController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowController : MonoBehaviour {
+
+    Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Open() {
+        anim.SetTrigger("Open");
+    }
+
+    public void Close() {
+        anim.SetTrigger("Close");
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Open();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Close();
+        }
+    }
+
+}
